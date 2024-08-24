6c9b95f6c83940c8df6fe551f4c6ae985abde1b0
Akshay kumar singh <akshaysing975@gmail.com>
2024-08-24 10:59:49 +0530

    some changes

--- routes/subscription.js
+++ routes/subscription.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Subscription = require('../models/Subscription'); // Import the Subscription model
+const Subscription = require('../models/Subscription');
 const { sendSubscriptionEmail } = require('../utils/mailer');
 
 router.post('/subscribe', async (req, res) => {
